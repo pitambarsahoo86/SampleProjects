ee122a02920dcddc6604b145ded203dd13093bc0
pitambarsahoo86 <pitambar.bs@gmail.com>
2020-05-13 13:00:58 +0530

    Project cleaned up

---
+++ WCF/WelcomeToWCF/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("WelcomeToWCF")]
+[assembly: AssemblyProduct("WelcomeToWCF")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+    )]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
